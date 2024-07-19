45d061a193be6963e3897bbdd6f1aa5639fa1b5c
ferdyanggara <ferdy@connect.ust.hk>
2024-07-19 16:29:06 +0800

    remove sensitive

--- get_rest_api/outputs.tf
+++ get_rest_api/outputs.tf
@@ -1,4 +1,4 @@
 output "api_gateway_invoke_url" {
   description = "Invoke URL for the API Gateway"
-  value       = "https://${data.aws_api_gateway_rest_api.example.id}.execute-api.ca-central-1.amazonaws.com"
+  value       = "https://${data.aws_api_gateway_rest_api.example.id}.execute-api.us-east-1.amazonaws.com"
 }
